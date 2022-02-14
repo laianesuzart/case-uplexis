040818ad3bb1d259d7ff4b1d112e5ede8c9184de
Laiane Suzart <laiane@pm.me>
2022-02-14 01:02:13 -0300

    feat: filter apps

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,37 @@
 import React, { useState } from "react";
+import { useMediaQuery } from "react-responsive";
+import { RiArrowDownSFill, RiArrowUpSFill } from "react-icons/ri";
+import { FaGlobe } from "react-icons/fa";
 import styles from "../../styles/pages/home.module.scss";
 import { apps } from "../../shared/utils/mockedApps";
+import { icons } from "../../shared/utils/icons";
 import { Card } from "../../components/card";
 
 export function Home() {
   const [displayedApps, setDisplayedApps] = useState(apps);
+  const [isVisible, setIsVisible] = useState(false);
+  const [activeCategory, setActiveCategory] = useState("Todos");
+
+  const isNotMobile = useMediaQuery({ query: "(min-width: 768px)" });
+
+  const categories = [
+    "Profissional",
+    "Reguladores",
+    "Sócio Ambiental",
+    "Jurídico",
+    "Listas Restritivas",
+    "Mídia / Internet",
+    "Bens e Imóveis",
+    "Cadastro",
+    "Financeiro",
+  ];
+
+  function filterList(category: string) {
+    const filteredApps = apps.filter((app, index) => app.category === category);
+    setActiveCategory(category);
+    setDisplayedApps(filteredApps);
+  }
+
   function orderList(e: React.ChangeEvent<HTMLSelectElement>) {
     if (e.target.value === "price") {
       const sortedApps = [...displayedApps].sort((a, b) =>
@@ -20,21 +47,60 @@ export function Home() {
   }
 
   return (
-    <div className="full-container">
-      <div className={styles.wrapper}>
-        <label htmlFor="order-select">Ordenar</label>
-        <select name="order" id="order-select" onChange={(e) => orderList(e)}>
-          <option value="release">Lançamento</option>
-          <option value="price">Preço</option>
-        </select>
+    <main className="full-container">
+      <div className={styles.container}>
+        <div className={styles.filterContainer}>
+          <button
+            className={styles.toogleButton}
+            onClick={() => setIsVisible(!isVisible)}
+          >
+            Filtrar {isVisible ? <RiArrowUpSFill /> : <RiArrowDownSFill />}
+          </button>
+          {(isVisible || isNotMobile) && (
+            <ul className={styles.filterList}>
+              <li key="Todos">
+                <button
+                  className={`${styles.filter__button} ${
+                    activeCategory === "Todos" && styles.active
+                  }`}
+                  onClick={() => {
+                    setDisplayedApps(apps);
+                    setActiveCategory("Todos");
+                  }}
+                >
+                  <FaGlobe /> <span>Todos</span>
+                </button>
+              </li>
+              {categories.map((category, index) => (
+                <li key={category}>
+                  <button
+                    className={`${styles.filter__button} ${
+                      activeCategory === category && styles.active
+                    }`}
+                    onClick={() => filterList(category)}
+                  >
+                    {icons[category]} <span>{category}</span>
+                  </button>
+                </li>
+              ))}
+            </ul>
+          )}
+        </div>
+        <div className={styles.wrapper}>
+          <label htmlFor="order-select">Ordenar</label>
+          <select name="order" id="order-select" onChange={(e) => orderList(e)}>
+            <option value="release">Lançamento</option>
+            <option value="price">Preço</option>
+          </select>
+        </div>
       </div>
-      <ul className={styles.container}>
+      <ul className={styles.cardContainer}>
         {displayedApps.map((app, index) => (
           <li key={app.id}>
             <Card app={app} />
           </li>
         ))}
       </ul>
-    </div>
+    </main>
   );
 }
